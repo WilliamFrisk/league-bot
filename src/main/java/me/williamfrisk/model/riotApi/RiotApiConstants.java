84be56640e0470be34ad04f7ec634319b6a7f39f
William <william.hp.frisk@gmail.com>
2023-09-05 20:16:00 +0200

    Added pilot functionality

---
+++ src/main/java/me/williamfrisk/model/riotApi/RiotApiConstants.java
@@ -0,0 +1,6 @@
+package me.williamfrisk.model.riotApi;
+
+public class RiotApiConstants {
+    public static final String CURRENT_KEY =  "RGAPI-c054a1ce-0658-4f68-a5d9-994756cb42f8";
+    public static final String HOST = "https://euw1.api.riotgames.com";
+}
